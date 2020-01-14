27f8015e724dac407b607e0103627ea69865e0a3
Ayomide Onalaja <ayo.onalaja@gmail.com>
2020-01-14 04:23:54 -0500

    working with modals

--- screens/Home.js
+++ screens/Home.js
@@ -4,11 +4,14 @@ import {
 	Text, 
 	View, 
 	FlatList, 
+	Modal,
 	TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global'
 import Card from '../shared/Card'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Home(props) {
+	const [modalOpen, setModalOpen] = useState(false)
 	const [reviews, setReviews] = useState([
     { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
     { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
@@ -21,6 +24,23 @@ export default function Home(props) {
 
 	return (
 		<View style={globalStyles.container}>
+			<Modal visible={modalOpen} animationType='slide' presentationStyle="pageSheet">
+				<View style={styles.modalContent}>
+					<Ionicons 
+						name='ios-close-circle'
+						size={35}
+						onPress={() => setModalOpen(false)}
+						style={{ color: '#fff' }}
+					/>
+					<Text>Modal</Text>
+				</View>
+			</Modal>
+			<Ionicons 
+				name='ios-add-circle'
+				size={50}
+				onPress={() => setModalOpen(true)}
+				style={styles.modalToggle}
+			/>
 			<FlatList
 				data={reviews}
 				renderItem={({ item }) => (
@@ -34,3 +54,16 @@ export default function Home(props) {
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	modalContent: {
+		flex: 1,
+		padding: 20,
+		backgroundColor: '#000'
+	},
+	modalToggle: {
+		marginBottom: 10,
+		color: '#fff',
+		alignSelf: 'center'
+	}
+})
